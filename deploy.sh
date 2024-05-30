3f91396e0987558eaa81d3186b1e14bc017038c4
zengkaiqiang <kaiqiang.zeng@lango-tech.cn>
2024-05-30 15:28:17 +0800

    更新静态页面的上传脚本

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,8 @@ git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@github.com:zengkaiqiang562/zengkaiqiang562.github.io.git master
+# git push -f git@github.com:zengkaiqiang562/zengkaiqiang562.github.io.git master
+git push -f https://zengkaiqiang562@github.com/zengkaiqiang562/zengkaiqiang562.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
